03be58453fd86e71d9cf98068969f363748f505f
mahimakp <mahimakpatel61@gmail.com>
2020-08-13 14:57:14 +0000

    commit

---
+++ StaticKeyword/StaticKeyword/Program.cs
@@ -0,0 +1,21 @@
+﻿using StaticKeywordClassLibrary;
+using System;
+
+namespace StaticKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   GlobalValue value = new GlobalValue();
+            //   value.increment();
+
+            StaticExample instanceVaiable = new StaticExample();
+            //instanceVaiable.count;
+
+            int counter = GlobalValue.increment();
+            
+
+        }
+    }
+}
---
+++ StaticKeyword/StaticKeywordClassLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticKeywordClassLibrary
+{
+    public class StaticExample
+    {
+        public int count = 10;
+
+    }
+    public static class GlobalValue
+    {
+
+        static int count = 1;
+           
+        public static int increment()
+        {
+            return count++;
+        }
+    }
+}
