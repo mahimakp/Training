38abf0b9eb96e78a3667c25d4f7856fe9a8bc7de
mahimakp <mahimakpatel61@gmail.com>
2020-08-13 14:39:28 +0000

    commit

---
+++ Generation/Generation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace Generation
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Blast it...");
+            Console.Read();
+            Console.WriteLine("Started");
+            for (int i =0; i<100000000; i++)
+            {
+
+                SolidBrush redBrush = new SolidBrush(Color.Red);
+               
+                
+            }
+            Console.Read();
+        }
+    }
+  
+}
