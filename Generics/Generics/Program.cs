5eb8d89016a090a9ed5e670e7e5f25c0e3b6453d
mahimakp <mahimakpatel61@gmail.com>
2020-08-13 13:33:44 +0000

    commit

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericExample<int> intAmount = new GenericExample<int>();
+
+            int paidAmount = 10000;
+            int AmountToBePaid = 10000;
+            bool resultInt = intAmount.Check(paidAmount, AmountToBePaid);
+
+            GenericExample<string> stringCheck = new GenericExample<string>();
+            string name1 = "Mahima";
+            string name2 = "Mahima";
+            bool resultString = stringCheck.Check(name1, name2);
+
+
+
+        }
+    }
+
+    public class GenericExample<T>
+    {
+        public bool Check(T value1, T value2)
+        {
+            if (value1.Equals(value2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
