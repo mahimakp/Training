81bfdefd435321747f416dd2c62f23aa33d06d5c
mahimakp <mahimakpatel61@gmail.com>
2020-08-20 13:11:28 +0000

    Commit

---
+++ MVCProject/PatientLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PatientLibrary
+{
+    public class Patient
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public List<PatientProblem> problems { get; set; }
+    }
+    public class PatientProblem
+    {
+        public string problem { get; set; }
+    }
+}
