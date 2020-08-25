c579db1fda46c03bb177254108386b89d5879fe8
mahimakp <mahimakpatel61@gmail.com>
2020-08-25 04:35:40 +0000

    commit

---
+++ MVCwithWillis/HospitalRepository/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatientLibrary;
+using System;
+
+namespace HospitalRepository
+{
+    public class HospitalDbContext : DbContext
+    {
+        //event or method
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Patient>()
+                .ToTable("tblPatient");
+        }
+        public DbSet<Patient> patients { get; set; }
+    }
+}
---
+++ MVCwithWillis/MVCwithWillis/Models/PatientAddViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PatientLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCwithWillis.Models
+{
+    public class PatientAddViewModel
+    {
+         
+        public List<Patient> allPatients { get; set; }
+        public Patient currentPatient { get; set; }
+        public List<PatientProblem> patientProblems { get; set; }
+        public PatientProblem currentProblem { get; set; }
+    }
+}
